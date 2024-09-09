38208c2c18a2afe54ad84a06d078accb377117af
Mayara <rrddamazio@gmail.com>
2024-09-08 21:09:00 -0300

    criar arquivos necessarios

---
+++ converteutf832.c
@@ -0,0 +1,16 @@
+/* Maria Eduarda da Fonseca  2212985 33WB */
+/* Mayara Ramos Damazio 2210833 33WC */
+
+#include "converteutf832.h"
+#include <stdio.h>
+
+
+int convUtf32p8(FILE *arquivo_entrada, FILE *arquivo_saida)
+{
+
+}
+
+int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida)
+{
+
+}
